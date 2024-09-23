971ec21eb94e330917d1c90cdf47498ef6f11905
Mateusz Pogoda <matimeusz123@gmail.com>
2024-09-23 21:02:16 +0200

    23.09

--- projekt_nextjs/app/components/ImageWithText.js
+++ projekt_nextjs/app/components/ImageWithText.js
@@ -1,6 +1,6 @@
 // components/ImageWithText.js
 function ImageWithText({ imageUrl, text }) {
     return (
-        <div><img src={imageUrl} alt={text} style={{ width: '100px', height: '100px' }} /><p>{text}</p></div>  );
+        <div><img src={imageUrl} alt={text} style={{ width: '500px', height: '300px' }} /><p>{text}</p></div>  );
     }export default ImageWithText;
     
--- projekt_nextjs/app/page.js
+++ projekt_nextjs/app/page.js
@@ -1,6 +1,9 @@
 import ItemList from "./components/ItemList";
 import TextBlock from "./components/TextBlock";
 import ImageWithText from "./components/ImageWithText";
+import SimpleForm from "./components/SimpleForm"
+import ConditionalDisplay from "./components/ConditionalDisplay";
+import ListWithObjects from "./components/ListWithObjects";
 import styles from "./globals.css"
 
 export default function Home() {
@@ -13,9 +16,17 @@ export default function Home() {
 
         <TextBlock heading={"Test"} content={"Content"} />
 
-        <ImageWithText imageUrl={"https://shorturl.at/wk0mc"} text = {"Mercedes G-class"}  />
-        
+        <ImageWithText imageUrl={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Festival_automobile_international_2020_-_Bugatti_La_Voiture_Noire_-_001.jpg/800px-Festival_automobile_international_2020_-_Bugatti_La_Voiture_Noire_-_001.jpg"} text = {"Bugatti"}  />
 
+        <SimpleForm label="Wpisz coś:" placeholder="Wpisz coś..."/>
+        
+        <ConditionalDisplay isVisible={true}/>
+
+        <ListWithObjects items={[
+          {name: "buty", value:"Nike Air Force 1"},
+          {name: "sklep",value:"Biedronka"},
+          {name: "samochód",value:"Ford Focus"}
+        ]} ></ListWithObjects>
 
       </main>
   
